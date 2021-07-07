092c44c3c0e285c860a1a89059b0b232e15c435c
Vaidehi Phirke <vaidehiphirke@fb.com>
2021-07-07 02:37:19 -0500

    added logout

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -2,9 +2,13 @@ package com.example.instagram;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.instagram.databinding.ActivityMainBinding;
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,5 +17,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         final ActivityMainBinding mainBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(mainBinding.getRoot());
+
+        final Button btnLogout = mainBinding.btnLogout;
+        btnLogout.setOnClickListener(new LogoutButtonViewOnClickListener());
+    }
+
+    private void goToLoginActivity() {
+        final Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public class LogoutButtonViewOnClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            ParseUser.logOut();
+            goToLoginActivity();
+        }
     }
 }
